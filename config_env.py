a3d0271563c32f09f584eace8c79f4f378d973e3
Jesus Hincapie <jahlon@gmail.com>
2022-11-11 15:57:40 -0500

    Fix activation of virtual environment

--- config_env.py
+++ config_env.py
@@ -3,7 +3,7 @@ import platform
 import subprocess
 
 if __name__ == "__main__":
-    # Enable script execution
+    # Try to enable script execution
     powershell64 = os.path.join(os.environ['SystemRoot'],
                                 'SysNative' if platform.architecture()[0] == '32bit' else 'System32',
                                 'WindowsPowerShell', 'v1.0', 'powershell.exe')
@@ -20,11 +20,13 @@ if __name__ == "__main__":
 
     if venv is None:
         # create virtual environment
-        os.system("python -m venv .venv")
-        venv = ".venv"
+        os.system("python -m venv venv")
+        venv = "venv"
 
     # activate virtual environment
-    os.system(f".\\{venv}\\Scripts\\activate")
+    activate_this_file = f".\\{venv}\\Scripts\\activate_this.py"
+    with open(activate_this_file) as f:
+        exec(f.read(), dict(__file__=activate_this_file))
 
     # install requirements
     os.system("pip install -r requirements.txt")
