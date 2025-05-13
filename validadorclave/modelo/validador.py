719b8ddda1a9c68b74441d8481c3bfb4dbfcd10d
juan arango <juanesarangocalle9@gmail.com>
2025-05-12 22:48:19 -0500

    agregar métodos de validación comunes en clase base ReglaValidacion

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -7,3 +7,22 @@ from .errores import (
     NoTieneCaracterEspecialError,
     NoTienePalabraSecretaError
 )
+class ReglaValidacion(ABC):
+    def _init_(self, longitud_esperada):
+        self._longitud_esperada = longitud_esperada
+
+    @abstractmethod
+    def es_valida(self, clave):
+        pass
+
+    def _validar_longitud(self, clave):
+        return len(clave) > self._longitud_esperada
+
+    def _contiene_mayuscula(self, clave):
+        return any(c.isupper() for c in clave)
+
+    def _contiene_minuscula(self, clave):
+        return any(c.islower() for c in clave)
+
+    def _contiene_numero(self, clave):
+        return any(c.isdigit() for c in clave)
