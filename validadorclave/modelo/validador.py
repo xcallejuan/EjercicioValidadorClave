20ad30724b0574ca54a168a72aaa7e67ad47b3c7
juan arango <juanesarangocalle9@gmail.com>
2025-05-12 22:50:46 -0500

    implementar regla de validación Ganimedes con verificación de caracteres especiales

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -26,3 +26,23 @@ class ReglaValidacion(ABC):
 
     def _contiene_numero(self, clave):
         return any(c.isdigit() for c in clave)
+    class ReglaValidacionGanimedes(ReglaValidacion):
+        def _init_(self):
+            super()._init_(8)
+
+        def contiene_caracter_especial(self, clave):
+            return any(c in "@_#$%" for c in clave)
+
+        def es_valida(self, clave):
+            if not self._validar_longitud(clave):
+                raise NoCumpleLongitudMinimaError("La clave debe tener una longitud de más de 8 caracteres")
+            if not self._contiene_mayuscula(clave):
+                raise NoTieneLetraMayusculaError("La clave debe contener al menos una letra mayúscula")
+            if not self._contiene_minuscula(clave):
+                raise NoTieneLetraMinusculaError("La clave debe contener al menos una letra minúscula")
+            if not self._contiene_numero(clave):
+                raise NoTieneNumeroError("La clave debe contener al menos un número")
+            if not self.contiene_caracter_especial(clave):
+                raise NoTieneCaracterEspecialError(
+                    "La clave debe contener al menos un carácter especial (@, _, #, $ o %)")
+            return True
